550193557c24e5a5abc6a44f5ef685436748e1ac
Punvireakroth <vireakrothpun@gmail.com>
2022-04-27 23:56:42 +0700

    Refactored Code and added new table sytle

--- app.py
+++ app.py
@@ -8,148 +8,106 @@
 ******************************************************************************
 '''
 
-print('''
+from tracemalloc import start
+#class colors
+class color:
+        PURPLE = '\033[95m'
+        CYAN = '\033[96m'
+        DARKCYAN = '\033[36m'
+        BLUE = '\033[94m'
+        GREEN = '\033[92m'
+        YELLOW = '\033[93m'
+        RED = '\033[91m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
+        END = '\033[0m' 
+# Start 
+start_over = 'true'
+while start_over == 'true':
+    print('''
 
-                    ░█████╗░░█████╗░███╗░░░███╗██████╗░░█████╗░██╗░░░██╗███╗░░██╗██████╗░
-                    ██╔══██╗██╔══██╗████╗░████║██╔══██╗██╔══██╗██║░░░██║████╗░██║██╔══██╗
-                    ██║░░╚═╝██║░░██║██╔████╔██║██████╔╝██║░░██║██║░░░██║██╔██╗██║██║░░██║
-                    ██║░░██╗██║░░██║██║╚██╔╝██║██╔═══╝░██║░░██║██║░░░██║██║╚████║██║░░██║
-                    ╚█████╔╝╚█████╔╝██║░╚═╝░██║██║░░░░░╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
-                    ░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚═╝░░░░░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+                        ░█████╗░░█████╗░███╗░░░███╗██████╗░░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+                        ██╔══██╗██╔══██╗████╗░████║██╔══██╗██╔══██╗██║░░░██║████╗░██║██╔══██╗
+                        ██║░░╚═╝██║░░██║██╔████╔██║██████╔╝██║░░██║██║░░░██║██╔██╗██║██║░░██║
+                        ██║░░██╗██║░░██║██║╚██╔╝██║██╔═══╝░██║░░██║██║░░░██║██║╚████║██║░░██║
+                        ╚█████╔╝╚█████╔╝██║░╚═╝░██║██║░░░░░╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+                        ░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚═╝░░░░░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
 
-                    ██╗███╗░░██╗████████╗███████╗██████╗░███████╗░██████╗████████╗
-                    ██║████╗░██║╚══██╔══╝██╔════╝██╔══██╗██╔════╝██╔════╝╚══██╔══╝
-                    ██║██╔██╗██║░░░██║░░░█████╗░░██████╔╝█████╗░░╚█████╗░░░░██║░░░
-                    ██║██║╚████║░░░██║░░░██╔══╝░░██╔══██╗██╔══╝░░░╚═══██╗░░░██║░░░
-                    ██║██║░╚███║░░░██║░░░███████╗██║░░██║███████╗██████╔╝░░░██║░░░
-                    ╚═╝╚═╝░░╚══╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚══════╝╚═════╝░░░░╚═╝░░░
-''')
+                        ██╗███╗░░██╗████████╗███████╗██████╗░███████╗░██████╗████████╗
+                        ██║████╗░██║╚══██╔══╝██╔════╝██╔══██╗██╔════╝██╔════╝╚══██╔══╝
+                        ██║██╔██╗██║░░░██║░░░█████╗░░██████╔╝█████╗░░╚█████╗░░░░██║░░░
+                        ██║██║╚████║░░░██║░░░██╔══╝░░██╔══██╗██╔══╝░░░╚═══██╗░░░██║░░░
+                        ██║██║░╚███║░░░██║░░░███████╗██║░░██║███████╗██████╔╝░░░██║░░░
+                        ╚═╝╚═╝░░╚══╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚══════╝╚═════╝░░░░╚═╝░░░
+    ''')
 
-#Get user input 
-A = float(input("                    How much money you have to save into account each month? "))
-N = int(input("                    How many month you want to put your money? "))
-W = float(input("                    How much interest you earn annually? "))
-print("\n\n")
-w = W / 1200 # interest for each month
+    #Get user input 
+    A = float(input("How much money you have to save into account each month? "))
+    N = int(input("How many month you want to put your money? "))
+    W = float(input("How much interest you earn annually? "))
+    print("\n\n")
+    w = W / 1200 # interest for each month
 
-# Color 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-# print(f"{bcolors.OKGREEN}============================================================================================================={bcolors.ENDC}")
-
-
-# print(f"{bcolors.OKBLUE}Month{bcolors.ENDC}\t {bcolors.WARNING}Deposit{bcolors.ENDC}\t {bcolors.OKGREEN}Total Deposits{bcolors.ENDC}\t {bcolors.OKCYAN}This Month's Interest{bcolors.ENDC}\t {bcolors.HEADER}Total-Interest Earned{bcolors.ENDC}\t {bcolors.FAIL}Total-Value To Date{bcolors.ENDC}")
-# print(f"{bcolors.OKGREEN}============================================================================================================={bcolors.ENDC}")
-#=================Calculate month=========================================== 
-
-def cal_month():
-    month_list = []
-    for time in range(1,N+1):
-        month = month_list.append(time)
-    month_list.append(month)
-    month_list.pop()
-    return month_list
-# print(cal_month())
-
-#---------Print Deposit ------------------------------------------
-def cal_deposit():
-    deposit_list = []
-    for time in range(1,N+1):
-        deposit = deposit_list.append(format(A,".2f"))
-    deposit_list.append(deposit)
-    deposit_list.pop()
-    return deposit_list
-# print(cal_deposit())
-#---------Total Deposit ------------------------------------------
-def cal_total_deposit():
-    total_deposit_list = []
-    for time in range(1,N+1):
-        total_deposit = total_deposit_list.append(format(A*time,".2f"))
-    total_deposit_list.append(total_deposit)
-    total_deposit_list.pop()
-    return total_deposit_list
-# print(cal_total_deposit())
-#-------- Calculate This Month's interest ------------------------
-
-def cal_month_interest():
-    counter = 0 
-    first_monthly_interest = [] 
-    first_monthly_interest.append(A/100)
-    for time in range(1,N+1):
-        M = (1+w)**time
-        counter += M
-        total_val_to_date = A* counter
-        this_month_interest = format((total_val_to_date + 100)/100,".2f")
-        first_monthly_interest.append(this_month_interest)
-    first_monthly_interest.append(first_monthly_interest)
-    first_monthly_interest.pop()
-    first_monthly_interest.pop()
-    return first_monthly_interest
-# print(cal_month_interest())
-#-----------------------calculate total interest earned ------------------------
-def cal_total_interest_earned():
-    counter = 0 
-    total_interest_earned_list = []
-    for time in range(1,N+1):
-        M = (1+w)**time
-        counter += M
-        total_val_to_date = A* counter
-        total_interest_earned = total_interest_earned_list.append(format(total_val_to_date - (A*time),".2f"))
-    total_interest_earned_list.append(total_interest_earned)
-    total_interest_earned_list.pop()
-    return total_interest_earned_list
+    # initial value function 
+    def cal_compound_interest(A,N,w):
+        F = A*(1+w)**N
+        compound_interest = F - A
+        return compound_interest
+    # function that handle with add and remove item to the list 
+    def add_remove_list(list_value, value):
+        for j in range(1):
+            assign = list_value.append(value)
+        list_value.append(assign)
+        list_value.pop()
+        
     
-# print(cal_total_interest_earned())
-#--------------------Calculate Total value to date -----------------------
-def cal_total_value_to_date():
-    counter = 0
-    total_value_to_date_list = []
+    total_value_to_date = 0
+
+    list_month = []
+    list_deposit = []
+    list_total_deposit = []
+    list_this_month_interest = []
+    list_total_interest_earned = []
+    list_total_value_to_date = []
+
     for time in range(1,N+1):
-        M = (1+w)**time
-        counter += M
-        total_val_to_date =total_value_to_date_list.append(format(A * counter,".2f"))
-    total_value_to_date_list.append(total_val_to_date)
-    total_value_to_date_list.pop()
-    return total_value_to_date_list
-# print(cal_total_value_to_date())
-#-----------------------------Display Result as a table------------------------------------------
+        month = time 
+        total_deposit = A * time
+        this_month_interest = cal_compound_interest(A,time,w)
+        total_value_to_date += A*(1+w)**time
+        total_interest_earned = total_value_to_date - total_deposit
 
-import pandas as pd
-import numpy as np
+        # --------------------------append value to the list -----------------------------------
+        #Month
+        add_remove_list(list_month,month)
+        #Deposit
+        add_remove_list(list_deposit,A)
+        #Total Deposit
+        add_remove_list(list_total_deposit,total_deposit)
+        #This month interest 
+        add_remove_list(list_this_month_interest,this_month_interest)
+        #Total interest Earned 
+        add_remove_list(list_total_interest_earned,total_interest_earned)
+        #Total value to date
+        add_remove_list(list_total_value_to_date,total_value_to_date)
+   
+    #---------------------------Print table--------------------------------------------
+    
+    from beautifultable import BeautifulTable
+    table = BeautifulTable()
+    table.columns.append(list_month, header=color.BOLD + color.BLUE + "Month")
+    table.columns.append(list_deposit, header=color.BOLD + color.BLUE +"Deposit")
+    table.columns.append(list_total_deposit, header=color.BOLD + color.BLUE +"Total-Deposit")
+    table.columns.append(list_this_month_interest, header=color.BOLD + color.BLUE +"This-Month-interest")
+    table.columns.append(list_total_interest_earned, header=color.BOLD + color.BLUE+"Total-Interest-Earned")
+    table.columns.append(list_total_value_to_date, header=color.BOLD + color.BLUE +"Total-Value-to-Date")
+    table.set_style(BeautifulTable.STYLE_BOX_ROUNDED)
+    print(table)
 
-#Insert label
-labels = []
-bold_labels = []
-for j in range(1,N+1):
-    new_labels = labels.append("█")
-    new_bold_labels = bold_labels.append("░")
-labels.append(new_labels)
-labels.pop()
-bold_labels.append(new_bold_labels)
-bold_labels.pop()
+    redo_program = input(color.RED+"\n\nDo you want to calculate your Compound interest again? (Y/N) : ").upper()
+    if (redo_program == 'Y'):
+        start_over = 'true'
+    else:
+        start_over = 'false'
 
-exam_data  = {
-        'Month': cal_month(),
-        'Deposit':cal_deposit(),
-        'Total Deposits':cal_total_deposit(),
-        'This Month Interest':cal_month_interest(),
-        'Total-Interest Earned': cal_total_interest_earned(),
-        'Total-Value To Date': cal_total_value_to_date(),
-        '█': labels,
-        '░': bold_labels
-        }
-
-
-df = pd.DataFrame(exam_data,index=labels)
-print(f"███████████████████████████████████████████████████████████████████████████████████████████████████████████████")
-print(df[['Month','█', 'Deposit','░', 'Total Deposits','█', 'This Month Interest','░', 'Total-Interest Earned','░', 'Total-Value To Date','█']])
-print(f"███████████████████████████████████████████████████████████████████████████████████████████████████████████████")
 
